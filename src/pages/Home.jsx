d513276de1a1e48716bb54f40aac14daa4794b77
malu31 <maluvenancio.3107@gmail.com>
2024-06-06 10:01:00 -0300

    rotas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/react.svg'
+
+const Home = () => {
+
+    return (
+        <img src={logo} width="300" alt="logo do React" />
+    )
+}
+
+export default Home
---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,10 @@
+const Inicio = () => {
+
+return (
+    <> 
+    <H2>Página de Início</H2>
+    </>
+)
+}
+
+export default Inicio
